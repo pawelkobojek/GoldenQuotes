fdf84e0b66bc2e2bd1c005f62ca38b80ddd94705
Paweł <PawelKobojek@gmail.com>
2015-02-10 15:28:58 +0100

    refactoring: removed unused imports

--- app/src/main/groovy/pl/antipattern/goldenquotes/ui/BaseActivity.groovy
+++ app/src/main/groovy/pl/antipattern/goldenquotes/ui/BaseActivity.groovy
@@ -3,7 +3,7 @@ package pl.antipattern.goldenquotes.ui
 import android.os.Bundle
 import android.support.v7.app.ActionBarActivity
 import groovy.transform.CompileStatic
-import pl.antipattern.goldenquotes.dagger.Injector;
+import pl.antipattern.goldenquotes.dagger.Injector
 
 @CompileStatic
 abstract class BaseActivity extends ActionBarActivity {
--- app/src/main/groovy/pl/antipattern/goldenquotes/ui/MainActivity.groovy
+++ app/src/main/groovy/pl/antipattern/goldenquotes/ui/MainActivity.groovy
@@ -1,16 +1,8 @@
 package pl.antipattern.goldenquotes.ui
 
-import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import groovy.transform.CompileStatic
-import groovy.transform.PackageScope
 import pl.antipattern.goldenquotes.R
-import pl.antipattern.goldenquotes.data.model.Author
-import pl.antipattern.goldenquotes.data.model.Quote
-
-import javax.inject.Inject
-
-import static nl.qbusict.cupboard.CupboardFactory.cupboard
 
 @CompileStatic
 class MainActivity extends BaseActivity {
