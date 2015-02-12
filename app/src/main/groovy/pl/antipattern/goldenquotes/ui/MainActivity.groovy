917bfaa989b4dd875aaaf6de51f82cce16e416f8
Paweł <PawelKobojek@gmail.com>
2015-02-12 13:04:24 +0100

    switched to Toolbar

--- app/src/main/groovy/pl/antipattern/goldenquotes/ui/MainActivity.groovy
+++ app/src/main/groovy/pl/antipattern/goldenquotes/ui/MainActivity.groovy
@@ -1,6 +1,8 @@
 package pl.antipattern.goldenquotes.ui
 
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import groovy.transform.CompileStatic
 import pl.antipattern.goldenquotes.R
 
@@ -11,6 +13,7 @@ class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar = findViewById(R.id.toolbar) as Toolbar
 
         supportFragmentManager.beginTransaction()
                 .add(R.id.container, new RandomQuoteFragment())
